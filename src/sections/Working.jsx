559035432545bff8d462d6fb92bc07e5298c4ffd
MosandosSantos <mosansantos@yahoo.com.br>
2025-10-22 17:40:13 -0300

    The Sun8

--- src/sections/Working.jsx
+++ src/sections/Working.jsx
@@ -12,7 +12,7 @@ const Working = () => {
         variants={slideUpVariants}
         className='lg:w-[80%] w-[90%] m-auto py-[60px] flex flex-col justify-between items-center gap-[20px]'>
         <motion.h1 variants={slideUpVariants} className=' text-yellow-500 text-2xl'>Sobre Nós</motion.h1>
-        <motion.h1 variants={slideUpVariants} className='text-black uppercase text-[40px] font-bold text-center'>Principais Setores da RSBarranpm</motion.h1>
+        <motion.h1 variants={slideUpVariants} className='text-black uppercase text-[40px] font-bold text-center'>Principais Setores da RSBarra</motion.h1>
         <motion.div variants={slideUpVariants} className='w-[120px] h-[6px] bg-yellow-500'></motion.div>
         <motion.div
           initial="hidden"
